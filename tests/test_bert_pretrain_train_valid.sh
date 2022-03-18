dba2c7a1823e9eb84b0dada315eb0a4222cbf110
Yura Kuratov <9271630+yurakuratov@users.noreply.github.com>
2022-03-18 22:07:37 +0300

    tests: scripts to test t5 and bert pretraining

---
+++ tests/test_bert_pretrain_train_valid.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+cd ..
+
+horovodrun --gloo -np $NP python run_bert_pretraining.py \
+--data_path ./data/toy_wiki/train_text_sentence --valid_data_path ./data/toy_wiki/valid_text_sentence \
+--tokenizer ./vocabs/bert_base_uncased/ --model_cfg ./bert_configs/bert_base_uncased-4L.json \
+--model_path ./tests/runs/test_bert_pretrain \
+--batch_size 16 --gradient_accumulation_steps 2 \
+--data_n_epochs 1 --lr 3e-04 --save_best --iters 100 --log_interval 25 --save_interval 50 --valid_interval 50
+echo "run_bert_pretraining.py done"
+echo "cleaning..."
+rm -rf ./tests/runs/test_bert_pretrain
+echo "done"
