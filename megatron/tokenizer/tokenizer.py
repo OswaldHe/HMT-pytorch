dd8f55b5c09cee37fd0208e5b4e99f9bd229c76f
Yura Kuratov <9271630+yurakuratov@users.noreply.github.com>
2022-07-14 12:58:10 +0300

    fix: megatron HF tokenizer should not add special tokens

--- megatron/tokenizer/tokenizer.py
+++ megatron/tokenizer/tokenizer.py
@@ -335,7 +335,8 @@ class _HFAutoTokenizer(AbstractTokenizer):
         return self.decoder
 
     def tokenize(self, text):
-        return self.tokenizer.encode(text)
+        # special tokens will be added in BertDataset
+        return self.tokenizer.encode(text, add_special_tokens=False)
 
     def detokenize(self, token_ids):
         return self.tokenizer.decode(token_ids)
