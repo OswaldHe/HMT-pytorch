5620cccd10ff41967f864efd3e100ee979df046b
Yura Kuratov <9271630+yurakuratov@users.noreply.github.com>
2022-08-11 12:00:56 +0300

    feat: add show_valid_examples arg to scrolls tasks

--- scripts/finetune_scrolls_contract_nli.sh
+++ scripts/finetune_scrolls_contract_nli.sh
@@ -31,30 +31,33 @@ TGT_LEN=1024
 
 METRIC=exact_match
 SCHEDULER=linear
+ITERS=9000
+TBS=16
+BS=4
 
 for (( i=0; i<${#MODEL_NAMES[@]}; i++ ))
 do
 MODEL_NAME=${MODEL_NAMES[i]}
 MODEL_CLS=${MODEL_CLSS[i]}
-for SRC_LEN in 256 512 1024
+for SRC_LEN in 1024
 do
-for LR in 2e-04 1e-04 5e-05 2e-05 1e-05
+for LR in 1e-04 5e-05 2e-05
 do
 for N in 1 2 3
 do
 horovodrun --gloo -np $NP python run_finetuning_scrolls.py \
         --task_name $TASK_NAME \
-        --model_path ./runs/finetune/$TASK_NAME/$MODEL_NAME/lr${LR}_${SCHEDULER}_adamw_wd1e-03_${SRC_LEN}-${TGT_LEN}/run_$N \
+        --model_path ./runs/finetune/$TASK_NAME/$MODEL_NAME/lr${LR}_${SCHEDULER}_adamw_wd1e-03_${SRC_LEN}-${TGT_LEN}_bs${TBS}_iters${ITERS}/run_$N \
         --from_pretrained $MODEL_NAME \
         --model_type $MODEL_TYPE \
         --model_cls transformers:${MODEL_CLS}ForConditionalGeneration \
         --use_generate_on_valid \
         --input_seq_len $SRC_LEN \
         --target_seq_len $TGT_LEN \
-        --batch_size 8 --gradient_accumulation_steps 2 \
-        --iters 4500 \
+        --batch_size $BS --gradient_accumulation_steps $(($TBS/($BS*$NP))) \
+        --iters $ITERS \
         --optimizer AdamW  --weight_decay 0.001 \
-        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps 450 \
+        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/10)) \
         --data_n_workers 2 \
         --log_interval 100 --valid_interval 225 \
         --optimize_metric $METRIC --optimize_mode max \
