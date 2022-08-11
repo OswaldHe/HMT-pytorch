3f1862c70746e53b34275e4d8b1ced13b6782905
Yura Kuratov <9271630+yurakuratov@users.noreply.github.com>
2022-08-11 18:40:27 +0300

    fix: remove duplicates in scrolls qasper valid set

--- scripts/finetune_scrolls_qasper.sh
+++ scripts/finetune_scrolls_qasper.sh
@@ -32,34 +32,38 @@ TGT_LEN=1024
 METRIC=f1
 SCHEDULER=linear
 
+ITERS=1600
+TBS=32
+BS=8
+
 for (( i=0; i<${#MODEL_NAMES[@]}; i++ ))
 do
 MODEL_NAME=${MODEL_NAMES[i]}
 MODEL_CLS=${MODEL_CLSS[i]}
-for SRC_LEN in 256 512 1024
+for LR in 1e-04 5e-05 2e-05
 do
-for LR in 2e-04 1e-04 5e-05 2e-05 1e-05
+for SRC_LEN in 256 512 1024
 do
 for N in 1 2 3
 do
 horovodrun --gloo -np $NP python run_finetuning_scrolls.py \
         --task_name $TASK_NAME \
-        --model_path ./runs/finetune/$TASK_NAME/$MODEL_NAME/lr${LR}_${SCHEDULER}_adamw_wd1e-03_${SRC_LEN}-${TGT_LEN}/run_$N \
+        --model_path ./runs/finetune/$TASK_NAME/$MODEL_NAME/lr${LR}_${SCHEDULER}_adamw_wd1e-03_${SRC_LEN}-${TGT_LEN}_bs${TBS}_iters${ITERS}/run_$N \
         --from_pretrained $MODEL_NAME \
         --model_type $MODEL_TYPE \
         --model_cls transformers:${MODEL_CLS}ForConditionalGeneration \
         --use_generate_on_valid \
         --input_seq_len $SRC_LEN \
         --target_seq_len $TGT_LEN \
-        --batch_size 4 --gradient_accumulation_steps 4 \
-        --iters 1600 \
+        --batch_size $BS --gradient_accumulation_steps $(($TBS/($BS*$NP))) \
+        --iters $ITERS \
         --optimizer AdamW  --weight_decay 0.001 \
-        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps 160 \
+        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/10)) \
         --data_n_workers 2 \
-        --log_interval 100 --valid_interval 100 \
+        --log_interval $(($ITERS/20)) --valid_interval $(($ITERS/20)) \
         --optimize_metric $METRIC --optimize_mode max \
-        --seed $(($N+42)) \
-        --clip_grad_value 5.0
+        --show_valid_examples 5 \
+        --seed $(($N+42))
 done
 done
 done
