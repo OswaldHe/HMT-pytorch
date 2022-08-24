6525d90ed9d07fce1ae6c3d1754bc4492e052e19
booydar <ayd98@mail.ru>
2022-08-24 22:21:41 +0300

    cleanup+segment ordering+padding side+inter-layer

--- modeling_rmt_enc_dec.py
+++ modeling_rmt_enc_dec.py
@@ -2,12 +2,8 @@ import torch
 import torch.nn.functional as F
 from typing import List, Optional, Tuple, Union
 from transformers import PreTrainedModel, AutoModel
-# from modeling_t5 import T5ForConditionalGeneration
 
 import math
-
-SEGMENTABLE = ['input_ids', 'inputs_embeds', 'token_type_ids', 'position_ids', 'attention_mask']
-PAD_ZEROS = ['token_type_ids', 'attention_mask']
 
 
 class RMTEncoderDecoderForConditionalGeneration():
@@ -21,7 +17,6 @@ class RMTEncoderDecoderForConditionalGeneration():
 
 
     def from_pretrained(from_pretrained, **kwargs):
-        # print(f'Creating from pretrained: {from_pretrained}')
         base_model = AutoModel.from_pretrained(from_pretrained, **kwargs)
         rmt = RMTEncoderDecoderForConditionalGeneration(base_model=base_model)
         rmt.from_pretrained = from_pretrained
@@ -29,36 +24,32 @@ class RMTEncoderDecoderForConditionalGeneration():
         
 
     def set_params(self, 
-                    model_attr='', 
-                    drop_empty_segments=True,
-                    sum_loss=False,
                     backbone_cls=None,
+                    num_mem_tokens=0, 
+                    inter_layer_memory=False,
+                    segment_ordering='regular',
+                    padding_side = 'left',
                     input_size=None, 
                     input_seg_size=None, 
-                    num_mem_tokens=0, 
                     bptt_depth=-1, 
-                    pad_token_id=0, 
-                    eos_token_id=1,
-                    cls_token_id=101, 
-                    sep_token_id=102):
-        # print('model attr: ', model_attr)
+                    drop_empty_segments=True,
+                    sum_loss=False,
+                    tokenizer=None):
 
         if backbone_cls is not None:
             self.model = backbone_cls.from_pretrained(self.from_pretrained)
 
-        if model_attr:
-            self.encoder = getattr(self.model, model_attr).encoder
-        else:
-            self.encoder = self.model.encoder
+        self.encoder = self.model.encoder
         self.embeddings = self.encoder.embed_tokens
         self.input_size = self.embeddings.weight.shape[0] if input_size is None else input_size
         self.input_seg_size = input_seg_size
 
         self.bptt_depth = bptt_depth
-        self.pad_token_id = pad_token_id
-        self.eos_token = torch.tensor([eos_token_id])
-        # print(pad_token_id, eos_token_id)
+        self.pad_token_id = tokenizer.pad_token_id
+        self.eos_token = torch.tensor([tokenizer.eos_token_id])
         self.num_mem_tokens = num_mem_tokens
+        self.segment_ordering = segment_ordering
+        self.padding_side = padding_side
         self.drop_empty_segments = drop_empty_segments
         self.sum_loss = sum_loss
         self.extend_word_embeddings()
@@ -67,10 +58,7 @@ class RMTEncoderDecoderForConditionalGeneration():
     def set_memory(self, memory=None):
         if memory is None:
             mem_token_ids = self.mem_token_ids.to(device=self.device)
-            # print('setting memory')
-            # print('mem_token_ids', mem_token_ids.shape)
             memory = self.embeddings(mem_token_ids)
-            # print('memory', memory.shape)
         return memory
     
     def extend_word_embeddings(self):
@@ -79,17 +67,26 @@ class RMTEncoderDecoderForConditionalGeneration():
         self.mem_token_ids = torch.arange(vocab_size, vocab_size + self.num_mem_tokens)
         self.resize_token_embeddings(extended_vocab_size)
         self.embeddings = self.encoder.embed_tokens
-        # print('extended_vocab_size', extended_vocab_size)
-        # print('self.embeddings', self.embeddings.weight.shape)
-        # print('self.mem_token_ids', self.mem_token_ids)
 
 
     def __call__(self, input_ids, **kwargs):
         memory = self.set_memory()
         segmented = self.pad_and_segment(input_ids)
+        segmented = list(zip(*segmented))
+        
+        if self.segment_ordering in {'regular', 'last_memory_only'}:
+            pass
+        elif self.segment_ordering == 'reversed':
+            segmented = segmented[::-1]
+        elif self.segment_ordering == 'bidirectional':
+            segmented = segmented + segmented[::-1][1:]
+        elif self.segment_ordering == 'repeat_first':
+            segmented = segmented + segmented[:1]
+        else:
+            raise ValueError(f'Unknown segment ordering: {segment_ordering}')
 
         outputs = []
-        for seg_num, segment_data in enumerate(zip(*segmented)):
+        for seg_num, segment_data in enumerate(segmented):
             input_ids, attention_mask, token_type_ids = segment_data
             if memory.ndim == 2:
                 memory = memory.repeat(input_ids.shape[0], 1, 1)
@@ -123,24 +120,40 @@ class RMTEncoderDecoderForConditionalGeneration():
                 memory[non_empty_mask] = out.encoder_hidden_states[-1][:, :self.num_mem_tokens]
             else:
                 memory = out.encoder_hidden_states[-1][:, :self.num_mem_tokens]
-            # print('out', out.keys())
-            # print('memory3',  memory.shape)
+
+        for i, o in enumerate(outputs):
+            out[f'loss_{i}'] = o['loss'].mean()
 
         if self.sum_loss:
             out['loss'] = torch.stack([o['loss'] for o in outputs]).sum(dim=-1)
-
+        
         return out
 
 
     def generate(self, input_ids, **kwargs):
         memory = self.set_memory()
-        segmented = self.pad_and_segment(input_ids)
         min_length, max_length = None, None
         if 'min_length' in kwargs:
             min_length = kwargs.pop('min_length')
         if 'max_length' in kwargs:
             max_length = kwargs.pop('max_length')
-        for seg_num, segment_data in enumerate(zip(*segmented)):
+        
+        segmented = self.pad_and_segment(input_ids)
+        segmented = list(zip(*segmented))
+        
+        if self.segment_ordering in {'regular', 'last_memory_only'}:
+            pass
+        elif self.segment_ordering == 'reversed':
+            segmented = segmented[::-1]
+        elif self.segment_ordering == 'bidirectional':
+            segmented = segmented + segmented[::-1][1:]
+        elif self.segment_ordering == 'repeat_first':
+            segmented = segmented + segmented[:1]
+        else:
+            raise ValueError(f'Unknown segment ordering: {self.segment_ordering}')
+        
+        outputs = []
+        for seg_num, segment_data in enumerate(segmented):
             input_ids, attention_mask, token_type_ids = segment_data
             if memory.ndim == 2:
                 memory = memory.repeat(input_ids.shape[0], 1, 1)
@@ -188,20 +201,18 @@ class RMTEncoderDecoderForConditionalGeneration():
 
         augmented_inputs = []
         for input in input_ids:
-            # print('input != self.pad_token_id ', (input != self.pad_token_id).shape, (input != self.pad_token_id).sum())
-            # 1/0
             input = input[input != self.pad_token_id][1:-1]
 
-            seg_sep_inds = [0] + list(range(len(input), 0, -input_seg_size))[::-1] # chunk so that first segment has various size
+            if self.padding_side == 'left':
+                seg_sep_inds = [0] + list(range(len(input), 0, -input_seg_size))[::-1] # chunk so that first segment has various size
+            else:
+                seg_sep_inds = list(range(0, len(input), input_seg_size)) + [len(input)]
             input_segments = [input[s:e] for s, e in zip(seg_sep_inds, seg_sep_inds[1:])]
 
             def pad_add_special_tokens(tensor, seg_size):
                 tensor = torch.cat([
-                                    # self.cls_token.to(device=self.device),
                                     self.mem_token_ids.to(device=self.device),
-                                    # self.sep_token.to(device=self.device),
                                     tensor.to(device=self.device),
-                                    # self.sep_token.to(device=self.device),
                                     self.eos_token.to(device=self.device)
                                     ])
                 pad_size = seg_size - tensor.shape[0]
