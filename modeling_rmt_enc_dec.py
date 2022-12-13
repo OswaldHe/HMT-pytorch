348c3c3b3afd3ee5754b2e730461d025fce517a8
booydar <ayd98@mail.ru>
2022-12-13 18:45:35 +0300

    cleanup

--- modeling_rmt_enc_dec.py
+++ modeling_rmt_enc_dec.py
@@ -1,65 +1,23 @@
+import math
 import torch
 import torch.nn.functional as F
 from typing import List, Optional, Tuple, Union
 from transformers import PreTrainedModel, AutoModel
 
-import math
-
 
 class RMTEncoderDecoderForConditionalGeneration():
-    def __init__(self, config=None, base_model=None, **kwargs):
-        if config is not None:
-            self.config = config
-            self.model = AutoModel(config, **kwargs)
+    def __init__(self, base_model, **rmt_kwargs):
+        self.model = base_model
+        self.set_params(**rmt_kwargs)
+
+
+    def set_params(self, num_mem_tokens, tokenizer, **rmt_config):
+        self.rmt_config = rmt_config
+        self.extract_special_tokens(tokenizer)
+        self.extend_word_embeddings(num_mem_tokens)
         
-        if base_model is not None:
-            self.model = base_model
+        self.segment_size = rmt_config['input_size'] - num_mem_tokens - tokenizer.num_special_tokens_to_add()
 
-
-    def from_pretrained(from_pretrained, **kwargs):
-        base_model = AutoModel.from_pretrained(from_pretrained, **kwargs)
-        rmt = RMTEncoderDecoderForConditionalGeneration(base_model=base_model)
-        rmt.from_pretrained = from_pretrained
-        return rmt
-        
-
-    def set_params(self, 
-                    backbone_cls=None,
-                    model_attr=None,
-                    num_mem_tokens=0, 
-                    inter_layer_memory=False,
-                    segment_ordering='regular',
-                    padding_side = 'left',
-                    input_size=None, 
-                    input_seg_size=None, 
-                    bptt_depth=-1, 
-                    drop_empty_segments=True,
-                    sum_loss=False,
-                    tokenizer=None):
-
-        if backbone_cls is not None:
-            self.model = backbone_cls.from_pretrained(self.from_pretrained)
-
-        if model_attr is not None:
-            self.encoder = self.model.__getattr__(model_attr).encoder
-        else:
-            self.encoder = self.model.encoder
-        self.embeddings = self.encoder.embed_tokens
-        self.input_size = self.embeddings.weight.shape[0] if input_size is None else input_size
-        self.input_seg_size = input_seg_size
-
-        self.bptt_depth = bptt_depth
-        self.pad_token_id = tokenizer.pad_token_id
-        self.eos_token = torch.tensor([tokenizer.eos_token_id])
-        self.bos_token = torch.tensor([tokenizer.bos_token_id]) if 'bos_token' in tokenizer.special_tokens_map else None
-        self.num_special_tokens_to_add = tokenizer.num_special_tokens_to_add()
-        self.num_mem_tokens = num_mem_tokens
-        self.segment_ordering = segment_ordering
-        self.padding_side = padding_side
-        self.drop_empty_segments = drop_empty_segments
-        self.sum_loss = sum_loss
-        self.extend_word_embeddings()
-        
 
     def set_memory(self, memory=None):
         if memory is None:
@@ -67,195 +25,159 @@ class RMTEncoderDecoderForConditionalGeneration():
             memory = self.embeddings(mem_token_ids)
         return memory
     
-    def extend_word_embeddings(self):
-        vocab_size = self.embeddings.weight.shape[0]
-        extended_vocab_size = vocab_size + self.num_mem_tokens
-        self.mem_token_ids = torch.arange(vocab_size, vocab_size + self.num_mem_tokens)
+    
+    def extract_special_tokens(self, tokenizer):
+        self.pad_token_id = tokenizer.pad_token_id
+        self.eos_token = torch.tensor([tokenizer.eos_token_id])
+        self.bos_token = torch.tensor([tokenizer.bos_token_id]) if 'bos_token' in tokenizer.special_tokens_map else None
+    
+    
+    def extend_word_embeddings(self, num_mem_tokens):
+        vocab_size = self.model.encoder.embed_tokens.weight.shape[0]
+        extended_vocab_size = vocab_size + num_mem_tokens
+        self.num_mem_tokens = num_mem_tokens
+        self.mem_token_ids = torch.arange(vocab_size, vocab_size + num_mem_tokens)
         self.resize_token_embeddings(extended_vocab_size)
-        self.embeddings = self.encoder.embed_tokens
+        self.embeddings = self.model.encoder.embed_tokens
+        
+        mem_start_ind = 1 if self.bos_token is not None else 0
+        self.memory_position = range(mem_start_ind, mem_start_ind + num_mem_tokens)
 
 
     def __call__(self, input_ids, **kwargs):
         memory = self.set_memory()
-        mem_start_ind = 1 if self.bos_token is not None else 0
+        memory = memory.repeat(input_ids.shape[0], 1, 1)
         segmented = self.pad_and_segment(input_ids)
-        segmented = list(zip(*segmented))
         
-        if self.segment_ordering in {'regular', 'last_memory_only'}:
-            pass
-        elif self.segment_ordering == 'reversed':
-            segmented = segmented[::-1]
-        elif self.segment_ordering == 'bidirectional':
-            segmented = segmented + segmented[::-1][1:]
-        elif self.segment_ordering == 'repeat_first':
-            segmented = segmented + segmented[:1]
-        else:
-            raise ValueError(f'Unknown segment ordering: {self.segment_ordering}')
-
-        outputs = []
-        for seg_num, segment_data in enumerate(segmented):
-            input_ids, attention_mask, token_type_ids = segment_data
-            if memory.ndim == 2:
-                memory = memory.repeat(input_ids.shape[0], 1, 1)
-            if (self.bptt_depth > -1) and (len(segmented) - seg_num > self.bptt_depth): 
+        losses = []
+        for seg_num, segment_input_ids in enumerate(segmented):                
+            if (self.rmt_config['bptt_depth'] > -1) and (len(segmented) - seg_num > self.rmt_config['bptt_depth']): 
                 memory = memory.detach()
 
             seg_kwargs = dict(**kwargs)
-            if self.drop_empty_segments:
-                non_empty_mask = [not torch.equal(input_ids[i], self.empty) for i in range(len(input_ids))]
-                if sum(non_empty_mask) == 0:
-                    continue
-                input_ids = input_ids[non_empty_mask]
-                attention_mask = attention_mask[non_empty_mask]
-                token_type_ids = token_type_ids[non_empty_mask]
-                seg_kwargs['labels'] = seg_kwargs['labels'][non_empty_mask]
+            seg_kwargs['output_hidden_states'] = True
+            
+            non_empty_mask = [s is not None for s in segment_input_ids]
+            if sum(non_empty_mask) == 0:
+                continue
+            input_ids = torch.stack(segment_input_ids)[non_empty_mask]
+            attention_mask = self.get_attention_mask(input_ids)
+            token_type_ids = self.get_token_type_ids(input_ids)
+            seg_kwargs['labels'] = seg_kwargs['labels'][non_empty_mask]
 
-                inputs_embeds = self.embeddings(input_ids)
-                inputs_embeds[:, mem_start_ind:mem_start_ind+self.num_mem_tokens] = memory[non_empty_mask]
-
-            else:
-                inputs_embeds = self.embeddings(input_ids)
-                inputs_embeds[:, mem_start_ind:mem_start_ind+self.num_mem_tokens] = memory
+            inputs_embeds = self.embeddings(input_ids)
+            inputs_embeds[:, self.memory_position] = memory[non_empty_mask]
 
             seg_kwargs['inputs_embeds'] = inputs_embeds
             seg_kwargs['attention_mask'] = attention_mask
 
-            out = self.model.forward(**seg_kwargs, output_hidden_states=True)
-            outputs.append(out)
-            
-            if self.drop_empty_segments:
-                memory[non_empty_mask] = out.encoder_hidden_states[-1][:, mem_start_ind:mem_start_ind+self.num_mem_tokens]
-            else:
-                memory = out.encoder_hidden_states[-1][:, mem_start_ind:mem_start_ind+self.num_mem_tokens]
+            out = self.model.forward(**seg_kwargs)
+            memory[non_empty_mask] = out.encoder_hidden_states[-1][:, self.memory_position]
 
-        for i, o in enumerate(outputs):
-            out[f'loss_{i}'] = o['loss'].mean()
+            losses.append(out['loss'])
 
-        if self.sum_loss:
-            out['loss'] = torch.stack([o['loss'] for o in outputs]).sum(dim=-1)
+        # drop unnecessary hiddens to save memory
+        if not kwargs.get('output_hidden_states'):
+            for key in out.keys():
+                if 'hidden_state' in key:
+                    out[key] = None
+                    
+        for i, l in enumerate(losses):
+            out[f'loss_{i}'] = l.mean()
+
+        if self.rmt_config['sum_loss']:
+            out['loss'] = torch.stack(losses).sum(dim=0)
         
+        mem_token_ids = self.mem_token_ids.to(device=self.device)
+        memory_tokens = self.embeddings(mem_token_ids)
+
         return out
 
 
-    def generate(self, input_ids, **kwargs):
+    def generate(self, input_ids, **kwargs):        
         memory = self.set_memory()
-        mem_start_ind = 1 if self.bos_token is not None else 0
-        min_length, max_length = None, None
-        if 'min_length' in kwargs:
-            min_length = kwargs.pop('min_length')
-        if 'max_length' in kwargs:
-            max_length = kwargs.pop('max_length')
-        
+        memory = memory.repeat(input_ids.shape[0], 1, 1)
         segmented = self.pad_and_segment(input_ids)
-        segmented = list(zip(*segmented))
-        
-        if self.segment_ordering in {'regular', 'last_memory_only'}:
-            pass
-        elif self.segment_ordering == 'reversed':
-            segmented = segmented[::-1]
-        elif self.segment_ordering == 'bidirectional':
-            segmented = segmented + segmented[::-1][1:]
-        elif self.segment_ordering == 'repeat_first':
-            segmented = segmented + segmented[:1]
-        else:
-            raise ValueError(f'Unknown segment ordering: {self.segment_ordering}')
 
-        outputs = []
-        for seg_num, segment_data in enumerate(segmented):
-            input_ids, attention_mask, token_type_ids = segment_data
-            if memory.ndim == 2:
-                memory = memory.repeat(input_ids.shape[0], 1, 1)
-            if (self.bptt_depth > -1) and (len(segmented) - seg_num > self.bptt_depth): 
+        for seg_num, segment_input_ids in enumerate(segmented):                
+            if (self.rmt_config['bptt_depth'] > -1) and (len(segmented) - seg_num > self.rmt_config['bptt_depth']): 
                 memory = memory.detach()
 
             seg_kwargs = dict(**kwargs)
-            if self.drop_empty_segments:
-                non_empty_mask = [not torch.equal(input_ids[i], self.empty) for i in range(len(input_ids))]
-                if sum(non_empty_mask) == 0:
-                    continue
-                input_ids = input_ids[non_empty_mask]
-                attention_mask = attention_mask[non_empty_mask]
-                token_type_ids = token_type_ids[non_empty_mask]
+            seg_kwargs['output_hidden_states'] = True
+            non_empty_mask = [s is not None for s in segment_input_ids]
+            if sum(non_empty_mask) == 0:
+                continue
+            input_ids = torch.stack(segment_input_ids)[non_empty_mask]
+            attention_mask = self.get_attention_mask(input_ids)
+            token_type_ids = self.get_token_type_ids(input_ids)
 
-                inputs_embeds = self.embeddings(input_ids)
-                inputs_embeds[:, mem_start_ind:mem_start_ind+self.num_mem_tokens] = memory[non_empty_mask]
+            inputs_embeds = self.embeddings(input_ids)
+            inputs_embeds[:, self.memory_position] = memory[non_empty_mask]
 
-            else:
-                inputs_embeds = self.embeddings(input_ids)
-                inputs_embeds[:, mem_start_ind:mem_start_ind+self.num_mem_tokens] = memory
-                
             seg_kwargs['inputs_embeds'] = inputs_embeds
             seg_kwargs['attention_mask'] = attention_mask
-            if seg_num < len(segmented)-1:
-                labels = torch.zeros(inputs_embeds.shape[0], inputs_embeds.shape[1], device=inputs_embeds.device, dtype=input_ids.dtype)
-                out = self.model.forward(**seg_kwargs, output_hidden_states=True, labels=labels)
-                if self.drop_empty_segments:
-                    memory[non_empty_mask] = out.encoder_hidden_states[-1][:, mem_start_ind:mem_start_ind+self.num_mem_tokens]
-                else:
-                    memory = out.encoder_hidden_states[-1][:, mem_start_ind:mem_start_ind+self.num_mem_tokens]
+
+            if seg_num == len(segmented) - 1:
+                out = self.model.generate(**seg_kwargs)
             else:
-                out = self.model.generate(**seg_kwargs, output_hidden_states=True, min_length=min_length, max_length=max_length)
-            
+                for param in ['min_length', 'max_length']:
+                    if param in seg_kwargs:
+                        seg_kwargs.pop(param)
+                        
+                out = self.model.encoder(**seg_kwargs)
+                memory[non_empty_mask] = out.last_hidden_state[:, self.memory_position]
+        
         return out
 
-    def pad_and_segment(self, input_ids):
-        
-        sequence_len = input_ids.shape[1]
-        input_seg_size = self.input_size - self.num_mem_tokens - self.num_special_tokens_to_add
-        if self.input_seg_size is not None and self.input_seg_size < input_seg_size:
-            input_seg_size = self.input_seg_size
-            
-        n_segments = math.ceil(sequence_len / input_seg_size)
-
-        augmented_inputs = []
-        for input in input_ids:
-            input = input[input != self.pad_token_id][:-1]
+    def pad_and_segment(self, input_ids, **kwargs):       
+        segmented_batch = []
+        for seq in input_ids:
+            seq = seq[(seq != self.pad_token_id) & (seq != self.eos_token.item())]
             if self.bos_token is not None:
-                input = input[1:]
+                seq = seq[seq != self.bos_token_id]
+            seq = seq[:self.segment_size * self.rmt_config['max_n_segments']]
 
-            if self.padding_side == 'left':
-                seg_sep_inds = [0] + list(range(len(input), 0, -input_seg_size))[::-1] # chunk so that first segment has various size
-            else:
-                seg_sep_inds = list(range(0, len(input), input_seg_size)) + [len(input)]
-            input_segments = [input[s:e] for s, e in zip(seg_sep_inds, seg_sep_inds[1:])]
+            n_seg = math.ceil(len(seq) / self.segment_size)
+            input_segments = torch.chunk(seq, n_seg)
+            input_segments = [self.pad_add_special_tokens(t, self.rmt_config['input_size']) for t in input_segments]
 
-            def pad_add_special_tokens(tensor, seg_size):
-                input_elements = []
-                if self.bos_token is not None:
-                    input_elements.append(self.bos_token.to(device=self.device))
-
-                input_elements += [
-                                self.mem_token_ids.to(device=self.device),
-                                tensor.to(device=self.device),
-                                self.eos_token.to(device=self.device)
-                                ]
-
-                tensor = torch.cat(input_elements)
-                pad_size = seg_size - tensor.shape[0]
-                if pad_size > 0:
-                    tensor = F.pad(tensor, (0, pad_size))
-                return tensor
-
-            input_segments = [pad_add_special_tokens(t, self.input_size) for t in input_segments]
-            empty = torch.Tensor([]).int()
-            self.empty = pad_add_special_tokens(empty, self.input_size)
-            empty_segments = [self.empty for i in range(n_segments - len(input_segments))]
-            input_segments = empty_segments + input_segments
-
-            augmented_input = torch.cat(input_segments)
-            augmented_inputs.append(augmented_input)
-            
-        augmented_inputs = torch.stack(augmented_inputs)
-        attention_mask = torch.ones_like(augmented_inputs)
-        attention_mask[augmented_inputs == self.pad_token_id] = 0
-
-        token_type_ids = torch.zeros_like(attention_mask)
-
-        input_segments = torch.chunk(augmented_inputs, n_segments, dim=1)
-        attention_mask = torch.chunk(attention_mask, n_segments, dim=1)
-        token_type_ids = torch.chunk(token_type_ids, n_segments, dim=1)
+            segmented_batch.append(input_segments)
     
-        return input_segments, attention_mask, token_type_ids
+        # batch of segments -> segmented batch 
+        # + align segments to right border
+        # so that the last segment is always non-empty
+        segmented_batch = [[s[::-1][i] if len(s) > i else None for s in segmented_batch] \
+                            for i in range(self.rmt_config['max_n_segments'])][::-1]
+        return segmented_batch
+    
+    
+    def pad_add_special_tokens(self, tensor, segment_size):
+        input_elements = []
+        if self.bos_token is not None:
+            input_elements.append(self.bos_token.to(device=self.device))
+
+        input_elements += [
+                        self.mem_token_ids.to(device=self.device),
+                        tensor.to(device=self.device),
+                        self.eos_token.to(device=self.device)
+                        ]
+        tensor = torch.cat(input_elements)
+        
+        pad_size = segment_size - tensor.shape[0]
+        if pad_size > 0:
+            tensor = F.pad(tensor, (0, pad_size))                  
+        return tensor
+    
+    
+    def get_attention_mask(self, tensor):
+        mask = torch.ones_like(tensor)
+        mask[tensor == self.pad_token_id] = 0
+        return mask
+        
+    
+    def get_token_type_ids(self, tensor):
+        return torch.zeros_like(tensor)
 
 
     def to(self, device):
@@ -268,10 +190,3 @@ class RMTEncoderDecoderForConditionalGeneration():
 
     def __getattr__(self, attribute):
         return getattr(self.model, attribute)
-
-
-    def parameters(self, **kwargs):
-        return self.model.parameters(**kwargs)
-
-    def named_parameters(self, **kwargs):
-        return self.model.named_parameters(**kwargs)
