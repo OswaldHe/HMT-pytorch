052fee14ae7402f60be41a406c63047bfc2d7477
Yura Kuratov <9271630+yurakuratov@users.noreply.github.com>
2023-01-25 19:20:34 +0300

    feat: make rmt torch module

--- modeling_rmt.py
+++ modeling_rmt.py
@@ -1,54 +1,57 @@
 import math
 import torch
 import torch.nn.functional as F
-from typing import List, Optional, Tuple, Union
-from transformers import PreTrainedModel, AutoModel
 
 
-class RMTEncoderForSequenceClassification():
+class RMTEncoderForSequenceClassification(torch.nn.Module):
     def __init__(self, base_model, **rmt_kwargs):
+        super().__init__()
         self.model = base_model
         self.set_params(**rmt_kwargs)
-
 
     def set_params(self, num_mem_tokens, tokenizer, **rmt_config):
         self.rmt_config = rmt_config
         self.extract_special_tokens(tokenizer)
         self.extend_word_embeddings(num_mem_tokens)
-        
-        self.segment_size = rmt_config['input_size'] - num_mem_tokens - 3
 
+        self.segment_size = rmt_config['input_size'] - num_mem_tokens - 3
 
     def set_memory(self, memory=None):
         if memory is None:
-            mem_token_ids = self.mem_token_ids.to(device=self.device)
-            memory = self.embeddings(mem_token_ids)
+            mem_token_ids = self.mem_token_ids
+            memory = self.model.embeddings(mem_token_ids)
         return memory
-    
-    
+
     def extract_special_tokens(self, tokenizer):
         self.pad_token_id = tokenizer.pad_token_id
-        self.cls_token = torch.tensor([tokenizer.cls_token_id])
-        self.sep_token = torch.tensor([tokenizer.sep_token_id])
-    
-    
+        self.register_buffer('cls_token', torch.tensor([tokenizer.cls_token_id]))
+        self.register_buffer('sep_token', torch.tensor([tokenizer.sep_token_id]))
+
     def extend_word_embeddings(self, num_mem_tokens):
-        vocab_size = self.base_model.embeddings.word_embeddings.weight.shape[0]
+        vocab_size = self.model.base_model.embeddings.word_embeddings.weight.shape[0]
         extended_vocab_size = vocab_size + num_mem_tokens
         self.num_mem_tokens = num_mem_tokens
-        self.mem_token_ids = torch.arange(vocab_size, vocab_size + num_mem_tokens)
-        self.resize_token_embeddings(extended_vocab_size)
-        self.embeddings = self.base_model.embeddings.word_embeddings
-        
+        self.register_buffer('mem_token_ids', torch.arange(vocab_size, vocab_size + num_mem_tokens))
+        self.model.resize_token_embeddings(extended_vocab_size)
+        self.model.embeddings = self.model.base_model.embeddings.word_embeddings
+
         mem_start_ind = 1
         self.memory_position = range(mem_start_ind, mem_start_ind + num_mem_tokens)
 
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None,
+                inputs_embeds=None, labels=None, output_attentions=None, output_hidden_states=None, return_dict=None):
+        # todo: replace copy-pasted args with @functools.wraps(self.model.forward) decorator
+        # need to change Trainer's usage of inspect.getfullargspec to inspect.signature to support @wraps
+        kwargs = {'input_ids': input_ids, 'attention_mask': attention_mask, 'token_type_ids': token_type_ids,
+                  'position_ids': position_ids, 'head_mask': head_mask, 'inputs_embeds': inputs_embeds,
+                  'labels': labels, 'output_attentions': output_attentions,
+                  'output_hidden_states': output_hidden_states, 'return_dict': return_dict,
+                  }
 
-    def __call__(self, input_ids, **kwargs):
         memory = self.set_memory()
         memory = memory.repeat(input_ids.shape[0], 1, 1)
         segmented = self.pad_and_segment(input_ids)
-        
+
         losses = []
         for seg_num, segment_input_ids in enumerate(segmented):                
             if (self.rmt_config['bptt_depth'] > -1) and (len(segmented) - seg_num > self.rmt_config['bptt_depth']): 
@@ -56,7 +59,7 @@ class RMTEncoderForSequenceClassification():
 
             seg_kwargs = dict(**kwargs)
             seg_kwargs['output_hidden_states'] = True
-            
+
             non_empty_mask = [s is not None for s in segment_input_ids]
             if sum(non_empty_mask) == 0:
                 continue
@@ -65,14 +68,15 @@ class RMTEncoderForSequenceClassification():
             token_type_ids = self.get_token_type_ids(input_ids)
             seg_kwargs['labels'] = seg_kwargs['labels'][non_empty_mask]
 
-            inputs_embeds = self.embeddings(input_ids)
+            inputs_embeds = self.model.embeddings(input_ids)
             inputs_embeds[:, self.memory_position] = memory[non_empty_mask]
 
+            seg_kwargs['input_ids'] = None
             seg_kwargs['inputs_embeds'] = inputs_embeds
             seg_kwargs['attention_mask'] = attention_mask
             seg_kwargs['token_type_ids'] = token_type_ids
 
-            out = self.model.forward(**seg_kwargs)
+            out = self.model(**seg_kwargs)
             memory[non_empty_mask] = out.hidden_states[-1][:, self.memory_position]
 
             losses.append(out['loss'])
@@ -82,20 +86,19 @@ class RMTEncoderForSequenceClassification():
             for key in out.keys():
                 if 'hidden_state' in key:
                     out[key] = None
-                    
+
         for i, l in enumerate(losses):
             out[f'loss_{i}'] = l.mean()
 
         if self.rmt_config['sum_loss']:
             out['loss'] = torch.stack(losses).sum(dim=0)
-        
-        mem_token_ids = self.mem_token_ids.to(device=self.device)
-        memory_tokens = self.embeddings(mem_token_ids)
+
+        mem_token_ids = self.mem_token_ids
+        memory_tokens = self.model.embeddings(mem_token_ids)
 
         return out
 
-    
-    def pad_and_segment(self, input_ids):       
+    def pad_and_segment(self, input_ids):
         segmented_batch = []
         for seq in input_ids:
             seq = seq[(seq != self.pad_token_id) & (seq != self.cls_token.item()) & (seq != self.sep_token.item())]
@@ -106,49 +109,28 @@ class RMTEncoderForSequenceClassification():
             input_segments = [self.pad_add_special_tokens(t, self.rmt_config['input_size']) for t in input_segments]
 
             segmented_batch.append(input_segments)
-    
-        # batch of segments -> segmented batch 
+
+        # batch of segments -> segmented batch
         # + align segments to right border
         # so that the last segment is always non-empty
-        segmented_batch = [[s[::-1][i] if len(s) > i else None for s in segmented_batch] \
-                            for i in range(self.rmt_config['max_n_segments'])][::-1]
+        segmented_batch = [[s[::-1][i] if len(s) > i else None for s in segmented_batch]
+                           for i in range(self.rmt_config['max_n_segments'])][::-1]
         return segmented_batch
-    
-    
+
     def pad_add_special_tokens(self, tensor, segment_size):
         input_elements = []
-        input_elements += [
-                        self.cls_token.to(device=self.device),
-                        self.mem_token_ids.to(device=self.device),
-                        self.sep_token.to(device=self.device),
-                        tensor.to(device=self.device),
-                        self.sep_token.to(device=self.device)
-                        ]
+        input_elements += [self.cls_token, self.mem_token_ids, self.sep_token, tensor, self.sep_token]
         tensor = torch.cat(input_elements)
-        
+
         pad_size = segment_size - tensor.shape[0]
         if pad_size > 0:
-            tensor = F.pad(tensor, (0, pad_size))                  
+            tensor = F.pad(tensor, (0, pad_size))
         return tensor
-    
-    
+
     def get_attention_mask(self, tensor):
         mask = torch.ones_like(tensor)
         mask[tensor == self.pad_token_id] = 0
         return mask
-        
-    
+
     def get_token_type_ids(self, tensor):
         return torch.zeros_like(tensor)
-
-
-    def to(self, device):
-        self.model = self.model.to(device)
-        
-    
-    def cuda(self):
-        self.model.cuda()
-
-
-    def __getattr__(self, attribute):
-        return getattr(self.model, attribute)
