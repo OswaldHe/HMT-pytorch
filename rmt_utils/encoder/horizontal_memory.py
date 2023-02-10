b3d4151fa57c6fa7d64b4d091653ca42ea4c158c
booydar <ayd98@mail.ru>
2023-02-10 11:38:59 +0300

    encoder horizontal memory

--- rmt_utils/encoder/horizontal_memory.py
+++ rmt_utils/encoder/horizontal_memory.py
@@ -51,17 +51,21 @@ def horizontal_memory_forward(
             if i in rmt_parent.memory_storage:
                 layer_memory = rmt_parent.memory_storage[i]
                 non_empty_mask = rmt_parent.memory_storage['non_empty_mask']
-                if not all(non_empty_mask):
-                # if layer_memory.shape[0] > hidden_states.shape[0]:
-                    layer_memory = layer_memory[non_empty_mask]
                 if layer_memory.shape[0] == 1:
-                    layer_memory = layer_memory.repeat(hidden_states.shape[0], 1, 1)
+                    layer_memory = layer_memory.repeat(len(non_empty_mask), 1, 1)
                     
-                hidden_states = torch.cat([layer_memory, hidden_states], dim=1)
+                hidden_states = torch.cat([layer_memory[non_empty_mask], hidden_states], dim=1)
+                if attention_mask is not None:
+                    layer_attention_mask = torch.cat((attention_mask[:, :, :, :rmt_parent.num_mem_tokens], attention_mask), dim=-1)
+                else:
+                    layer_attention_mask = attention_mask
+            else:
+                layer_memory = None
+                layer_attention_mask = attention_mask
 
             layer_outputs = layer_module(
                 hidden_states,
-                attention_mask,
+                layer_attention_mask,
                 layer_head_mask,
                 encoder_hidden_states,
                 encoder_attention_mask,
@@ -74,25 +78,22 @@ def horizontal_memory_forward(
                 memory_layer = rmt_parent.memory_layers[i]
                 memory_layer_out = memory_layer(
                     hidden_states,
-                    attention_mask,
+                    layer_attention_mask,
                     layer_head_mask,
                     encoder_hidden_states,
                     encoder_attention_mask,
                     past_key_value,
                     output_attentions,
                 )
-                # memory = memory_layer_out[0][:, rmt_parent.memory_position]
 
         hidden_states = layer_outputs[0]
         
-        #!! shorten hidden states
+        ### shorten hidden states
         if i in rmt_parent.memory_storage:
             hidden_states = hidden_states[:, rmt_parent.num_mem_tokens:]
 
-        ### set new memory
+        ### update memory 
         if rmt_parent.memory_layers is not None:
-            # hidden_states[:, rmt_parent.memory_position] = memory_layer_out[0][:, rmt_parent.memory_position]
-        # else:
             memory_layer_hidden_states = memory_layer_out[0]
             if i in rmt_parent.memory_storage:
                 memory_layer_hidden_states = memory_layer_hidden_states[:, rmt_parent.num_mem_tokens:]
@@ -101,7 +102,11 @@ def horizontal_memory_forward(
             hidden_states[:, rmt_parent.memory_position] = updated_memory
         
         ### set layer memory
-        rmt_parent.memory_storage[i] = hidden_states[:, rmt_parent.memory_position].detach()
+        if layer_memory is not None:
+            layer_memory[non_empty_mask] = hidden_states[:, rmt_parent.memory_position].detach()
+        else:
+            layer_memory = hidden_states[:, rmt_parent.memory_position].detach()
+        rmt_parent.memory_storage[i] = layer_memory
 
         if use_cache:
             next_decoder_cache += (layer_outputs[-1],)
@@ -131,4 +136,114 @@ def horizontal_memory_forward(
         hidden_states=all_hidden_states,
         attentions=all_self_attentions,
         cross_attentions=all_cross_attentions,
+    )
+
+
+from collections.abc import Sequence
+from transformers.modeling_outputs import BaseModelOutput
+
+def deberta_memory_layers_forward(
+        self,
+        hidden_states,
+        attention_mask,
+        output_hidden_states=True,
+        output_attentions=False,
+        query_states=None,
+        relative_pos=None,
+        return_dict=True,
+        rmt_parent=None,
+    ):
+    attention_mask = self.get_attention_mask(attention_mask)
+    relative_pos = self.get_rel_pos(hidden_states, query_states, relative_pos)
+
+    all_hidden_states = () if output_hidden_states else None
+    all_attentions = () if output_attentions else None
+
+    if isinstance(hidden_states, Sequence):
+        next_kv = hidden_states[0]
+    else:
+        next_kv = hidden_states
+    rel_embeddings = self.get_rel_embedding()
+    for i, layer_module in enumerate(self.layer):
+
+        if output_hidden_states:
+            all_hidden_states = all_hidden_states + (hidden_states,)
+
+        if i in rmt_parent.memory_storage:
+            layer_memory = rmt_parent.memory_storage[i]
+            non_empty_mask = rmt_parent.memory_storage['non_empty_mask']
+            if layer_memory.shape[0] == 1:
+                layer_memory = layer_memory.repeat(len(non_empty_mask), 1, 1)
+                
+            hidden_states = torch.cat([layer_memory[non_empty_mask], hidden_states], dim=1)
+            if attention_mask is not None:
+                layer_attention_mask = torch.cat((attention_mask, attention_mask[:, :, :, :rmt_parent.num_mem_tokens]), dim=-1)
+            else:
+                layer_attention_mask = attention_mask
+        else:
+            layer_memory = None
+            layer_attention_mask = attention_mask
+
+        hidden_states = layer_module(
+            next_kv,
+            attention_mask=layer_attention_mask,
+            output_attentions=output_attentions,
+            query_states=query_states,
+            relative_pos=relative_pos,
+            rel_embeddings=rel_embeddings,
+        )
+        
+        ### Update memory
+        if rmt_parent.memory_layers is not None:
+            memory_layer = rmt_parent.memory_layers[i]
+            memory_layer_out = memory_layer(
+                next_kv,
+                attention_mask=layer_attention_mask,
+                output_attentions=output_attentions,
+                query_states=query_states,
+                relative_pos=relative_pos,
+                rel_embeddings=rel_embeddings,
+            )
+
+        if output_attentions:
+            hidden_states, att_m = hidden_states
+            memory_layer_out, memory_attentions = memory_layer_out
+        
+        ### shorten hidden states
+        if i in rmt_parent.memory_storage:
+            hidden_states = hidden_states[:, rmt_parent.num_mem_tokens:]
+
+        ### update memory 
+        if rmt_parent.memory_layers is not None:
+            memory_layer_hidden_states = memory_layer_out[0]
+            if i in rmt_parent.memory_storage:
+                memory_layer_hidden_states = memory_layer_hidden_states[:, rmt_parent.num_mem_tokens:]
+
+            updated_memory = memory_layer_hidden_states[:, rmt_parent.memory_position]
+            hidden_states[:, rmt_parent.memory_position] = updated_memory
+        
+        ### set layer memory
+        if layer_memory is not None:
+            layer_memory[non_empty_mask] = hidden_states[:, rmt_parent.memory_position].detach()
+        else:
+            layer_memory = hidden_states[:, rmt_parent.memory_position].detach()
+        rmt_parent.memory_storage[i] = layer_memory
+
+        if query_states is not None:
+            query_states = hidden_states
+            if isinstance(hidden_states, Sequence):
+                next_kv = hidden_states[i + 1] if i + 1 < len(self.layer) else None
+        else:
+            next_kv = hidden_states
+
+        if output_attentions:
+            all_attentions = all_attentions + (att_m,)
+
+    if output_hidden_states:
+        all_hidden_states = all_hidden_states + (hidden_states,)
+
+    if not return_dict:
+        return tuple(v for v in [hidden_states, all_hidden_states, all_attentions] if v is not None)
+    return BaseModelOutput(
+        last_hidden_state=hidden_states, hidden_states=all_hidden_states, attentions=all_attentions
     )
