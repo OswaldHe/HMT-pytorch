e9da554b516e13c3593e1a8df40887de75adfb1a
booydar <ayd98@mail.ru>
2023-02-10 12:51:01 +0300

    refactor token classification

--- modeling_rmt/token_classification.py
+++ modeling_rmt/token_classification.py
@@ -25,62 +25,79 @@ class RMTEncoderForTokenClassification(RMTEncoderForSequenceClassification):
                   'output_attentions': output_attentions, 'output_hidden_states': output_hidden_states,
                   'return_dict': return_dict,
                   }
+        memory = self.set_memory(input_ids.shape)
+        segmented = self.pad_and_segment(input_ids, labels, labels_mask)
+
+        base_model_outputs = []
+        for seg_num, segment in enumerate(zip(*segmented)):
+            if self.rmt_config['bptt_depth'] > -1:
+                raise NotImplementedError
+
+            seg_kwargs, non_empty_mask = self.prepare_kwargs(segment, kwargs)
+            seg_kwargs['inputs_embeds'][:, self.memory_position] = memory[non_empty_mask]
+
+            out = self.model(**seg_kwargs)
+            memory[non_empty_mask] = out.hidden_states[-1][:, self.memory_position]
+            
+            out['seg_kwargs'] = seg_kwargs
+            base_model_outputs.append(out)
+
+        out = self.process_outputs(input_ids, base_model_outputs, output_attentions, output_hidden_states)
+
+        return out
+
+
+    def prepare_kwargs(self, segment, kwargs):
+        segment_input_ids, segment_labels, segment_labels_mask = segment
+        seg_kwargs = dict(**kwargs)
+        non_empty_mask = [s is not None for s in segment_input_ids]
+        if sum(non_empty_mask) == 0:
+            return None, non_empty_mask
+            
+        input_ids = torch.stack([s for s in segment_input_ids if s is not None])
+        inputs_embeds = self.model.embeddings(input_ids)
+
+        seg_kwargs['input_ids'] = None
+        seg_kwargs['inputs_embeds'] = inputs_embeds
+        if seg_kwargs.get('labels') is not None:
+            seg_kwargs['labels'] = seg_kwargs['labels'][non_empty_mask]
+        seg_kwargs['attention_mask'] = self.get_attention_mask(input_ids)
+        if seg_kwargs.get('token_type_ids') is not None:
+            seg_kwargs['token_type_ids'] = self.get_token_type_ids(input_ids)
+        seg_kwargs['output_hidden_states'] = True
+        if seg_kwargs['labels'] is not None:
+            seg_kwargs['labels'] = torch.stack([el for el, m in zip(segment_labels, non_empty_mask) if m])
+        if seg_kwargs['labels_mask'] is not None:
+            seg_kwargs['labels_mask'] = torch.stack([el for el, m in zip(segment_labels_mask, non_empty_mask) if m])
+        if pos_weight is not None:
+            # all values in the second dimension of pos_weight should be the same
+            pos_weight = pos_weight[0, 0, :][None, None, :]
+            segm_bs, segm_seq_len, _ = seg_kwargs['labels'].shape
+            seg_kwargs['pos_weight'] = pos_weight.repeat(segm_bs, segm_seq_len, 1)
+
+        return seg_kwargs, non_empty_mask
+
+
+    def process_outputs(self, input_ids, model_outputs, output_attentions, output_hidden_states):
+        rmt_out = model_outputs[-1]
 
         bs, seq_len = input_ids.shape
 
-        memory = self.set_memory()
-        memory = memory.repeat(input_ids.shape[0], 1, 1)
-        segmented, segmented_labels, segmented_labels_mask = self.pad_and_segment(input_ids, labels, labels_mask)
-
+        seg_kwargs = out['seg_kwargs']
         losses = []
         logits = []
         logits_masks = []
         labels_segm = []
-        for seg_num, (segment_input_ids, segment_labels, segment_labels_mask) in enumerate(zip(segmented,
-                                                                                               segmented_labels,
-                                                                                               segmented_labels_mask)):
-            if (self.rmt_config['bptt_depth'] > -1) and (len(segmented) - seg_num > self.rmt_config['bptt_depth']):
-                memory = memory.detach()
-
-            seg_kwargs = dict(**kwargs)
-            seg_kwargs['output_hidden_states'] = True
-
-            non_empty_mask = [s is not None for s in segment_input_ids]
-            if sum(non_empty_mask) == 0:
-                continue
-            input_ids = torch.stack([s for s in segment_input_ids if s is not None])
-            attention_mask = self.get_attention_mask(input_ids)
-            token_type_ids = self.get_token_type_ids(input_ids)
-
-            inputs_embeds = self.model.embeddings(input_ids)
-            inputs_embeds[:, self.memory_position] = memory[non_empty_mask]
-
-            seg_kwargs['input_ids'] = None
-            seg_kwargs['inputs_embeds'] = inputs_embeds
-            seg_kwargs['attention_mask'] = attention_mask
-            seg_kwargs['token_type_ids'] = token_type_ids
-            if labels is not None:
-                seg_kwargs['labels'] = torch.stack([el for el, m in zip(segment_labels, non_empty_mask) if m])
-            if labels_mask is not None:
-                seg_kwargs['labels_mask'] = torch.stack([el for el, m in zip(segment_labels_mask, non_empty_mask) if m])
-            if pos_weight is not None:
-                # all values in the second dimension of pos_weight should be the same
-                pos_weight = pos_weight[0, 0, :][None, None, :]
-                segm_bs, segm_seq_len, _ = seg_kwargs['labels'].shape
-                seg_kwargs['pos_weight'] = pos_weight.repeat(segm_bs, segm_seq_len, 1)
-
-            out = self.model(**seg_kwargs)
-            memory[non_empty_mask] = out.hidden_states[-1][:, self.memory_position]
-
+        for out in model_outputs:
             losses.append(out['loss'])
             logits.append(out['logits'].detach())
             labels_segm += [seg_kwargs['labels']]
 
-            if labels_mask is not None:
+            if seg_kwargs['labels_mask'] is not None:
                 logits_masks.append(seg_kwargs['labels_mask'])
 
         # drop unnecessary hiddens to save memory
-        if not kwargs.get('output_hidden_states'):
+        if not output_hidden_states:
             for key in out.keys():
                 if 'hidden_state' in key:
                     out[key] = None
@@ -110,7 +127,7 @@ class RMTEncoderForTokenClassification(RMTEncoderForSequenceClassification):
             out['rmt_logits_masks'] = torch.cat(logits_masks, dim=1)
             out['rmt_logits_masks_segm'] = [logits_masks]
 
-        return out
+        return rmt_out 
 
     def pad_and_segment(self, input_ids, labels=None, labels_mask=None):
         segmented_batch = []
@@ -136,34 +153,46 @@ class RMTEncoderForTokenClassification(RMTEncoderForSequenceClassification):
                 labels_mask = labels_mask[content_tokens_mask]
                 labels_mask = labels_mask[:self.segment_size * self.rmt_config['max_n_segments']]
 
-            n_seg = math.ceil(len(seq) / self.segment_size)
-            input_segments = torch.chunk(seq, n_seg)
+            # n_seg = math.ceil(len(seq) / self.segment_size)
+            # input_segments = torch.chunk(seq, n_seg)
+            align = self.rmt_config.get('segment_alignment')
+            if align in {'right', None}:
+                split_inds = (list(range(len(seq), 0, -self.segment_size)) + [0])[::-1]
+            elif align == 'left':
+                split_inds = list(range(0, len(seq), self.segment_size)) + [len(seq)]
+            elif align == 'center':
+                n_seg = math.ceil(len(seq) / self.segment_size)
+                split_inds = list(range(0, len(seq), math.ceil(len(seq) / n_seg))) + [len(seq)]
+            else:
+                raise NotImplementedError
+
+            input_segments = [seq[start:end] for (start, end) in zip(split_inds, split_inds[1:])]
             input_segments = [self.pad_add_special_tokens(t, self.rmt_config['input_size']) for t in input_segments]
             segmented_batch.append(input_segments)
 
             if labels is not None:
-                labels_segments = torch.chunk(labels, n_seg)
+                labels_segments = [labels[start:end] for (start, end) in zip(split_inds, split_inds[1:])]
                 labels_segments = [self.pad_add_special_tokens(t, self.rmt_config['input_size'], add_to='labels') for t in labels_segments]
                 segmented_batch_labels.append(labels_segments)
 
             if labels_mask is not None:
-                labels_mask_segments = torch.chunk(labels_mask, n_seg)
+                labels_mask_segments = [labels_mask[start:end] for (start, end) in zip(split_inds, split_inds[1:])]
                 labels_mask_segments = [self.pad_add_special_tokens(t, self.rmt_config['input_size'], add_to='labels_mask') for t in labels_mask_segments]
                 segmented_batch_labels_mask.append(labels_mask_segments)
 
-        # batch of segments -> segmented batch
-        # + align segments to right border
-        # so that the last segment is always non-empty
-        segmented_batch = [[s[::-1][i] if len(s) > i else None for s in segmented_batch]
-                           for i in range(self.rmt_config['max_n_segments'])][::-1]
+        # # batch of segments -> segmented batch
+        # # + align segments to right border
+        # # so that the last segment is always non-empty
+        # segmented_batch = [[s[::-1][i] if len(s) > i else None for s in segmented_batch]
+        #                    for i in range(self.rmt_config['max_n_segments'])][::-1]
 
-        if len(segmented_batch_labels) > 0:
-            segmented_batch_labels = [[s[::-1][i] if len(s) > i else None for s in segmented_batch_labels]
-                                      for i in range(self.rmt_config['max_n_segments'])][::-1]
+        # if len(segmented_batch_labels) > 0:
+        #     segmented_batch_labels = [[s[::-1][i] if len(s) > i else None for s in segmented_batch_labels]
+        #                               for i in range(self.rmt_config['max_n_segments'])][::-1]
 
-        if len(segmented_batch_labels_mask) > 0:
-            segmented_batch_labels_mask = [[s[::-1][i] if len(s) > i else None for s in segmented_batch_labels_mask]
-                                           for i in range(self.rmt_config['max_n_segments'])][::-1]
+        # if len(segmented_batch_labels_mask) > 0:
+        #     segmented_batch_labels_mask = [[s[::-1][i] if len(s) > i else None for s in segmented_batch_labels_mask]
+        #                                    for i in range(self.rmt_config['max_n_segments'])][::-1]
 
         return segmented_batch, segmented_batch_labels, segmented_batch_labels_mask
 
