c5f1f24f4855ee20b9e882fd63da8ccca852659a
booydar <ayd98@mail.ru>
2023-02-10 11:39:46 +0300

    tests and result parsing

--- scripts/test_finetune_cnli_rmt.sh
+++ scripts/test_finetune_cnli_rmt.sh
@@ -7,12 +7,12 @@ CUBLAS_WORKSPACE_CONFIG=:4096:2
 CUDA_LAUNCH_BLOCKING=1
 
 MODEL_TYPE=encoder
-MODEL_NAMES=(microsoft/deberta-v3-base bert-base-cased roberta-base )
+MODEL_NAMES=(bert-base-cased roberta-base )
 BACKBONE_CLS=transformers:AutoModelForSequenceClassification
 TASK_NAME=contract_nli
 METRIC=exact_match
 
-ITERS=30
+ITERS=5
 TBS=32
 BS=2
 
@@ -41,6 +41,43 @@ SCHEDULER=linear
 
 for LR in 1e-05
 do
+
+
+MODEL_CLS=modeling_rmt:RMTEncoderMLMMemLoss
+
+RC=0.01
+echo RUNNING: TASK_NAME SRC_LEN MODEL_NAME MODEL_CLS N_SEG MEMORY_SIZE INPUT_SEQ_LEN LR N
+echo RUNNING: $TASK_NAME $SRC_LEN $MODEL_NAME $MODEL_CLS $MAX_N_SEGMENTS $MEMORY_SIZE $INPUT_SEQ_LEN $LR $N
+horovodrun --gloo -np $NP python run_finetuning_scrolls_rmt.py \
+        --task_name $TASK_NAME \
+        --model_path ../runs/framework/${TASK_NAME}/$MODEL_NAME/lr${LR}_${SCHEDULER}_adamw_wd1e-03_${INPUT_SEQ_LEN}-${TGT_LEN}-{$MAX_N_SEGMENTS}seg_mem${MEMORY_SIZE}_bs${TBS}_iters${ITERS}_${SEGMENT_ORDERING}_mem_layers_shared_mlm_mem_loss_rc${RC}/run_$N \
+        --from_pretrained $MODEL_NAME \
+        --model_type $MODEL_TYPE \
+        --model_cls $MODEL_CLS \
+        --backbone_cls $BACKBONE_CLS \
+        --input_seq_len $INPUT_SEQ_LEN \
+        --input_size 512 \
+        --target_seq_len $TGT_LEN \
+        --num_mem_tokens $MEMORY_SIZE \
+        --max_n_segments $MAX_N_SEGMENTS \
+        --memory_layers 'all' \
+        --share_memory_layers \
+        --reconstruction_loss_coef $RC \
+        --segment_ordering $SEGMENT_ORDERING \
+        --bptt_depth -1 \
+        --batch_size $BS --gradient_accumulation_steps $(($TBS/($BS*$NP))) \
+        --iters $ITERS \
+        --optimizer AdamW  --weight_decay 0.001 \
+        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/1)) \
+        --data_n_workers 2 \
+        --log_interval $(($ITERS/100)) --valid_interval $(($ITERS/1)) \
+        --optimize_metric $METRIC --optimize_mode max \
+        --show_valid_examples 5 \
+        --early_stopping_patience 15 \
+        --optimize_metric $METRIC --optimize_mode max \
+        --seed $(($N+42)) \
+        --clip_grad_value 5.0
+        
 MODEL_CLS=modeling_rmt:RMTEncoderMemoryLayers
 
 echo RUNNING: TASK_NAME SRC_LEN MODEL_NAME MODEL_CLS N_SEG MEMORY_SIZE INPUT_SEQ_LEN LR N
@@ -64,9 +101,9 @@ horovodrun --gloo -np $NP python run_finetuning_scrolls_rmt.py \
         --batch_size $BS --gradient_accumulation_steps $(($TBS/($BS*$NP))) \
         --iters $ITERS \
         --optimizer AdamW  --weight_decay 0.001 \
-        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/10)) \
+        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/1)) \
         --data_n_workers 2 \
-        --log_interval $(($ITERS/10)) --valid_interval $(($ITERS/2)) \
+        --log_interval $(($ITERS/10)) --valid_interval $(($ITERS/1)) \
         --optimize_metric $METRIC --optimize_mode max \
         --show_valid_examples 5 \
         --early_stopping_patience 15 \
@@ -96,9 +133,9 @@ horovodrun --gloo -np $NP python run_finetuning_scrolls_rmt.py \
         --batch_size $BS --gradient_accumulation_steps $(($TBS/($BS*$NP))) \
         --iters $ITERS \
         --optimizer AdamW  --weight_decay 0.001 \
-        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/10)) \
+        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/1)) \
         --data_n_workers 2 \
-        --log_interval $(($ITERS/10)) --valid_interval $(($ITERS/2)) \
+        --log_interval $(($ITERS/10)) --valid_interval $(($ITERS/1)) \
         --optimize_metric $METRIC --optimize_mode max \
         --show_valid_examples 5 \
         --early_stopping_patience 15 \
