03229ae15f500748677e0a823459e9b1ae81f3cb
booydar <ayd98@mail.ru>
2023-02-18 11:27:32 +0300

    positional encoding mod + mem output variant + fix empty segments drop

--- modeling_rmt/conditional_generation.py
+++ modeling_rmt/conditional_generation.py
@@ -52,10 +52,9 @@ class RMTEncoderDecoderForConditionalGeneration(RMTBaseModel):
                 raise NotImplementedError
 
             seg_kwargs, non_empty_mask = self.prepare_kwargs(segment_input_ids, kwargs)
-            seg_kwargs['inputs_embeds'][:, self.memory_position] = memory[non_empty_mask]
             if sum(non_empty_mask) == 0:
                 continue
-
+            seg_kwargs['inputs_embeds'][:, self.memory_position] = memory[non_empty_mask]
             if seg_num == len(segmented) - 1:
                 out = self.model.generate(**seg_kwargs)
             else:
@@ -195,9 +194,9 @@ class RMTEncoderDecoderHorizontalMemory(RMTEncoderDecoderMemoryLayers):
                 raise NotImplementedError
 
             seg_kwargs, non_empty_mask = self.prepare_kwargs(segment_input_ids, kwargs)
-            seg_kwargs['inputs_embeds'][:, self.memory_position] = memory[non_empty_mask]
             if sum(non_empty_mask) == 0:
                 continue
+            seg_kwargs['inputs_embeds'][:, self.memory_position] = memory[non_empty_mask]
             self.memory_storage['non_empty_mask'] = non_empty_mask
 
             if seg_num == len(segmented) - 1:
@@ -209,5 +208,105 @@ class RMTEncoderDecoderHorizontalMemory(RMTEncoderDecoderMemoryLayers):
                         
                 out = self.model.encoder(**seg_kwargs)
                 memory[non_empty_mask] = out.last_hidden_state[:, self.memory_position]
+
+        return out
+    
+from torch.nn import CrossEntropyLoss
+class RMTEncoderDecoderMemoryOutput(RMTEncoderDecoderMemoryLayers):
+    def forward(self, input_ids, attention_mask=None, position_ids=None, head_mask=None,
+                inputs_embeds=None, labels=None, output_attentions=None, output_hidden_states=None, return_dict=None):
+        kwargs = {'attention_mask': attention_mask,
+                #   'position_ids': position_ids, 
+                  'inputs_embeds': inputs_embeds,
+                  'labels': labels, 'output_attentions': output_attentions,
+                  'output_hidden_states': output_hidden_states, 'return_dict': return_dict,
+                  }
+
+        memory = self.set_memory(input_ids.shape)
+        segmented = self.pad_and_segment(input_ids)
+
+        memories = []
+        base_model_outputs = []
+        for seg_num, segment_input_ids in enumerate(segmented):                
+            if self.rmt_config['bptt_depth'] != -1:
+                raise NotImplementedError
+
+            seg_kwargs, non_empty_mask = self.prepare_kwargs(segment_input_ids, kwargs)
+            if sum(non_empty_mask) == 0:
+                continue
+            
+            seg_kwargs['inputs_embeds'][:, self.memory_position] = memory[non_empty_mask]
+            out = self.model(**seg_kwargs)
+            base_model_outputs.append(out)
+            
+            memory[non_empty_mask] = out.encoder_hidden_states[-1][:, self.memory_position]
+            memories.append(torch.clone(memory))
+
+            if seg_num == len(segmented) - 1:
+                memories = torch.cat(memories, dim=1)
+                decoder_input_ids = self.model._shift_right(labels)
+
+                decoder_outputs = self.model.decoder(input_ids=decoder_input_ids, encoder_hidden_states=memories)
+                # base_model_outputs.append(out)
+
+        out = self.process_outputs(base_model_outputs, output_attentions, output_hidden_states)
+
+        sequence_output = decoder_outputs[0]
+        # Set device for model parallelism
+        if self.model.model_parallel:
+            torch.cuda.set_device(self.model.encoder.first_device)
+            self.model.lm_head = self.model.lm_head.to(self.model.encoder.first_device)
+            sequence_output = sequence_output.to(self.model.lm_head.weight.device)
+
+        if self.model.config.tie_word_embeddings:
+            # Rescale output before projecting on vocab
+            # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
+            sequence_output = sequence_output * (self.model.model_dim**-0.5)
+
+        lm_logits = self.model.lm_head(sequence_output)
+
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss(ignore_index=-100)
+            loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
+        
+        out['loss'] = loss
+
+        return out
+
+    def generate(self, input_ids, attention_mask=None, position_ids=None, head_mask=None,
+                inputs_embeds=None, output_attentions=None, output_hidden_states=None, return_dict=None,
+                min_length=None, max_length=None):
+        kwargs = {'attention_mask': attention_mask,
+                  'inputs_embeds': inputs_embeds,
+                  'output_attentions': output_attentions,
+                  'output_hidden_states': output_hidden_states, 'return_dict': return_dict,
+                  'min_length': min_length, 'max_length': max_length
+                  }
+
+        memory = self.set_memory(input_ids.shape)
+        segmented = self.pad_and_segment(input_ids)
+
+        memories = []
+        for seg_num, segment_input_ids in enumerate(segmented):                
+            if self.rmt_config['bptt_depth'] != -1:
+                raise NotImplementedError
+
+            seg_kwargs, non_empty_mask = self.prepare_kwargs(segment_input_ids, kwargs)
+            if sum(non_empty_mask) == 0:
+                continue
+            seg_kwargs['inputs_embeds'][:, self.memory_position] = memory[non_empty_mask]
+
+            for param in ['min_length', 'max_length']:
+                if param in seg_kwargs:
+                    seg_kwargs.pop(param)
+                    
+            out = self.model.encoder(**seg_kwargs)
+            memory[non_empty_mask] = out.last_hidden_state[:, self.memory_position]
+            memories.append(torch.clone(memory))
+
+            if seg_num == len(segmented) - 1:                
+                memories = torch.cat(memories, dim=1)
+                out = self.model.generate(**seg_kwargs, encoder_hidden_states=memories)
 
         return out
