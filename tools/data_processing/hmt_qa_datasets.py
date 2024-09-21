fa94b9a5d5f5823cbef9431271b6fdf50881a73b
Richard Cao <yic033@ucsd.edu>
2024-09-21 02:29:34 -0700

    Completed the fine tuning script and the QA fine tuning is currently available.

--- tools/data_processing/hmt_qa_datasets.py
+++ tools/data_processing/hmt_qa_datasets.py
@@ -39,19 +39,29 @@ def prepare_narrativeqa_dataset(dataset: Union[datasets.Dataset, tuple[datasets.
         """
         return {
             "text": [f"{prompt}\n{question_delimiter}{entry[0]['text']}\n{context_delimiter}{entry[1]['text']}\n{answer_delimiter}{entry[2][0]['text']}" for entry in zip(dataset['question'], dataset['document'], dataset['answers'])],
-            "answers": [f""]
             "answer_length": [len(entry[0]['text']) for entry in dataset['answers']]
         }
     
     prepared_data = ()
     if source == "deepmind":
         for split in dataset:
-            prepared_data = prepared_data + (split.map(nqa_entry_to_text, batched=True).remove_columns(['question', 'document', 'answers']),)
+            prepared_data = prepared_data + (split.map(nqa_entry_to_text, batched=True, 
+                                                       desc=f"Concatenating QA Input Questions and Contexts",
+                                                       num_proc=8).remove_columns(['question', 'document', 'answers']),)
     else:
         raise NotImplementedError(f"process dataset for source {source} not implemented.")
     return prepared_data
 
-def load_qa_dataset(name, **kwargs):
+def filter_by_length(dataset: tuple[datasets.Dataset], max_length: int=100000):
+    out = ()
+
+    for split in dataset:
+        split_ = split.filter(lambda x: len(x['document']['text']) < max_length)
+        assert len(split_) != 0, f"The number of samples with length < {max_length} is 0!"
+        out = out + (split_,)
+    return out
+
+def load_qa_dataset(name, max_context_length: int=None, **kwargs):
     """
     Load a question answering dataset from Hugging Face.
 
@@ -63,6 +73,8 @@ def load_qa_dataset(name, **kwargs):
     """
     if name == 'deepmind/narrativeqa':
         ds = load_narrativeqa_dataset(split=kwargs.get('split', 'test'), source='deepmind', streaming=kwargs.get('streaming', False))
+        if max_context_length is not None:
+            ds = filter_by_length(ds, max_context_length)
         ds = prepare_narrativeqa_dataset(ds, source='deepmind')
         if isinstance(ds, tuple) and len(ds) == 1:
             ds = ds[0]
