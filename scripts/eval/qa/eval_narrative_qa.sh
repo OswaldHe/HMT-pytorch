d7ac2e1a323b8011cb149accadd72c10880e7a0b
Richard Cao <yic033@ucsd.edu>
2024-10-04 16:14:06 -0700

    added narrative qa script.

--- scripts/eval/qa/eval_narrative_qa.sh
+++ scripts/eval/qa/eval_narrative_qa.sh
@@ -9,12 +9,6 @@ else
     conda activate llm
 fi
 
-if [ "$(hostname)" = "vastlab" ]; then export HF_HOME="/home/yingqi/scratch/head";
-elif [ "$(hostname)" = "c00" ]; then export HF_HOME="/home/yingqi/scratch/hf_home";
-elif [ "$(hostname)" = "c01" ]; then export HF_HOME="/home/yingqi/scratch/c01/cache";
-elif [ "$(hostname)" = "c02" ]; then export HF_HOME="/home/yingqi/scratch/c02/cache"; 
-elif [ "$(hostname)" = "c03" ]; then export HF_HOME="/home/yingqi/scratch/c03/cache"; fi
-
 # Verify the environment is activated
 if [ $? -eq 0 ]; then
     echo "Conda environment 'llm' activated successfully."
@@ -23,64 +17,117 @@ else
     exit 1
 fi
 
+
+if [ "$(hostname)" = "vastlab" ]; then export HF_HOME="/home/yingqi/scratch/head";
+elif [ "$(hostname)" = "c00" ]; then export HF_HOME="/home/yingqi/scratch/hf_home";
+elif [ "$(hostname)" = "c01" ]; then export HF_HOME="/home/yingqi/scratch/c01/cache";
+elif [ "$(hostname)" = "c02" ]; then export HF_HOME="/home/yingqi/scratch/c02/cache"; 
+elif [ "$(hostname)" = "c03" ]; then export HF_HOME="/home/yingqi/scratch/c03/cache"; fi
+
+
 cd /home/yingqi/repo/HMT-pytorch
 
 export NCCL_DEBUG=INFO
 export TORCH_DISTRIBUTED_DEBUG=INFO
+# export WANDB_MODE=offline
 
 accelerate env
 
-echo HF_HOME=$HF_HOME
+# Configure the task. Define the task specific parameters here, and the configuration of the models remain the change when evaluating different tasks. 
+export TASK_NAME=narrativeqa
+export MAX_NEW_TOKENS=128
 
-export WEIGHT_BASE=~/scratch/c00/hmt_pretrained/opt-350m
+# >>> SmolLM-135M >>>
+accelerate launch --main_process_port=29501 /home/yingqi/repo/HMT-pytorch/tools/evaluation/eval.py \
+    --learning_rate=1e-4 \
+    --model_name=HuggingFaceTB/SmolLM-135M \
+    --task_name=${TASK_NAME} \
+    --task_subset=sample \
+    --training_step=100 \
+    --num_sensory=32 \
+    --segment_length=1024 \
+    --bptt_depth=6 \
+    --train_set_split=2 \
+    --num_seg_save=8 \
+    --batch_size=1 \
+    --test_length=3000 \
+    --mem_recall_hidden_dim=1536 \
+    --test_step=200 \
+    --save_dir=/home/yingqi/scratch/checkpoints/opt-350m/qmsum \
+    --save_interval=10 \
+    --token_file=/home/yingqi/repo/HMT-pytorch/huggingface_token.txt \
+    --validation_interval=10 \
+    --curriculum \
+    --max_new_tokens=${MAX_NEW_TOKENS} \
+    --curriculum_segs=2,3,4,6,8 \
+    --wandb_entity=yic033-ucsd \
+    --wandb_run=baseline \
+    --wandb_project=qa_fine_tuning_evaluation \
+    --rouge \
+    --is_qa_task \
+    --max_context_length=16000000 \
+    --load_from_ckpt="/home/yingqi/scratch/hmt_pretrained/smollm-135m/model_weights_0_lv_2.pth"
+# <<< SmolLM-135M <<<
 
-checkpoints=(
-    model_weights_0_lv_2.pth
-)
+# >>> OPT-350M >>>
+accelerate launch /home/yingqi/repo/HMT-pytorch/tools/evaluation/eval.py \
+    --learning_rate=1e-4 \
+    --model_name=facebook/opt-350m \
+    --task_name=${TASK_NAME} \
+    --task_subset=sample \
+    --training_step=100 \
+    --num_sensory=32 \
+    --segment_length=1024 \
+    --bptt_depth=6 \
+    --train_set_split=2 \
+    --num_seg_save=8 \
+    --batch_size=1 \
+    --test_length=3000 \
+    --test_step=200 \
+    --save_dir=/home/yingqi/scratch/checkpoints/opt-350m/qmsum \
+    --save_interval=10 \
+    --token_file=/home/yingqi/repo/HMT-pytorch/huggingface_token.txt \
+    --validation_interval=10 \
+    --curriculum \
+    --curriculum_segs=2,3,4,6,8 \
+    --wandb_entity=yic033-ucsd \
+    --wandb_run=baseline \
+    --max_new_tokens=${MAX_NEW_TOKENS} \
+    --wandb_project=qa_fine_tuning_evaluation \
+    --rouge \
+    --is_qa_task \
+    --max_context_length=16000000 \
+    --load_from_ckpt="/home/yingqi/scratch/hmt_pretrained/opt-350m/model_weights_0_lv_2_step2.pth"
+# <<< OPT-350M <<<
 
-test_lengths=(3000)
-
-# FIXME: We need to modify the num_seg_save so that num_seg_save * 990 is at least as long as 
-# the length of the lables, or the number of tokens in the labels will be capped at this length. 
-# If the actual label length is loneger, then the size mismatch error will occur for cross-entropy loss.
-
-
-for test_length in "${test_lengths[@]}"; do
-    # remove the dataset cache
-    rm -rf /home/yingqi/scratch/c00/cache/grouped
-    rm -rf /home/yingqi/scratch/c00/cache/tokenized
-    for checkpoint in "${checkpoints[@]}"; do
-        accelerate launch tools/evaluation/eval.py \
-            --learning_rate=1e-4 \
-            --model_name=facebook/opt-350m \
-            --task_name=deepmind/narrativeqa \
-            --task_subset=sample \
-            --training_step=100 \
-            --num_sensory=32 \
-            --segment_length=1024 \
-            --bptt_depth=6 \
-            --train_set_split=2 \
-            --num_seg_save=8 \
-            --batch_size=1 \
-            --test_length=${test_length} \
-            --test_step=200 \
-            --save_dir=/home/yingqi/scratch/c00/checkpoints/rp_opt-350m \
-            --save_interval=10 \
-            --token_file=/home/yingqi/repo/HMT-pytorch/huggingface_token.txt \
-            --validation_interval=10 \
-            --validation_steps=10 \
-            --curriculum \
-            --curriculum_segs=2,3,4,6,8 \
-            --wandb_entity=yic033-ucsd \
-            --wandb_run=evaluate_narrative_qa_baseline \
-            --wandb_project=qa_fine_tuning \
-            --load_from_ckpt="/home/yingqi/scratch/c00/checkpoints/fine_tuning/opt-350m/narrative_qa/model_weights_580.pth" \
-            --is_qa_task    
-    done
-done
-
-# Available model names:
-# meta-llama/Llama-2-7b-hf
-# facebook/opt-350m
-
-# Valid task_subset: sample, sample-10B, sample-100B, sample-1T
+# >>> OpenLLaMA >>>
+accelerate launch /home/yingqi/repo/HMT-pytorch/tools/evaluation/eval.py \
+    --learning_rate=1e-4 \
+    --model_name=openlm-research/open_llama_3b_v2 \
+    --task_name=${TASK_NAME} \
+    --task_subset=sample \
+    --training_step=100 \
+    --use_lora \
+    --num_sensory=32 \
+    --segment_length=1024 \
+    --bptt_depth=6 \
+    --train_set_split=2 \
+    --num_seg_save=8 \
+    --batch_size=1 \
+    --test_length=3000 \
+    --test_step=200 \
+    --save_dir=/home/yingqi/scratch/checkpoints/opt-350m/qmsum \
+    --save_interval=10 \
+    --token_file=/home/yingqi/repo/HMT-pytorch/huggingface_token.txt \
+    --validation_interval=10 \
+    --curriculum \
+    --max_new_tokens=${MAX_NEW_TOKENS} \
+    --curriculum_segs=2,3,4,6,8 \
+    --wandb_entity=yic033-ucsd \
+    --wandb_run=baseline \
+    --wandb_project=qa_fine_tuning_evaluation \
+    --rouge \
+    --is_qa_task \
+    --max_context_length=16000000 \
+    --load_from_ckpt="/home/yingqi/scratch/hmt_pretrained/openllama_3b_v2/model_weights_0_lv_2.pth"
+# <<< OpenLLaMA <<<
